3212a6053743c84d15afa3d33d1816e79e075d5b
Yoga Fatwanto <95328326+Lulu-L2@users.noreply.github.com>
2023-02-07 10:17:02 +0700

    Update helper_function.py

--- helper_function.py
+++ helper_function.py
@@ -378,3 +378,23 @@ def plot_time_series(timesteps, values, format='.', start=0, end=None, label=Non
   if label:
     plt.legend(fontsize=14) # make label bigger
   plt.grid(True)
+
+
+
+def evaluate_preds(y_true, y_pred):
+  # Make sure float32 (for metric calculations)
+  y_true = tf.cast(y_true, dtype=tf.float32)
+  y_pred = tf.cast(y_pred, dtype=tf.float32)
+
+  # Calculate various metrics
+  mae = tf.keras.metrics.mean_absolute_error(y_true, y_pred)
+  mse = tf.keras.metrics.mean_squared_error(y_true, y_pred) # puts and emphasis on outliers (all errors get squared)
+  rmse = tf.sqrt(mse)
+  mape = tf.keras.metrics.mean_absolute_percentage_error(y_true, y_pred)
+  mase = mean_absolute_scaled_error(y_true, y_pred)
+  
+  return {"mae": mae.numpy(),
+          "mse": mse.numpy(),
+          "rmse": rmse.numpy(),
+          "mape": mape.numpy(),
+          "mase": mase.numpy()}
