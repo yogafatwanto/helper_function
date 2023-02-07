b2878a6b003f89cbc34e91c01889945d08cf0c38
Yoga Fatwanto <95328326+Lulu-L2@users.noreply.github.com>
2023-02-07 09:15:09 +0700

    Update helper_function.py

--- helper_function.py
+++ helper_function.py
@@ -357,3 +357,24 @@ def calculate_results(y_true, y_pred):
                   "recall": model_recall,
                   "f1": model_f1}
   return model_results
+
+def plot_time_series(timesteps, values, format='.', start=0, end=None, label=None):
+  """
+  Plots a timesteps (a series of points in time) against values (a series of values across timesteps).
+  
+  Parameters
+  ---------
+  timesteps : array of timesteps
+  values : array of values across time
+  format : style of plot, default "."
+  start : where to start the plot (setting a value will index from start of timesteps & values)
+  end : where to end the plot (setting a value will index from end of timesteps & values)
+  label : label to show on plot of values
+  """
+  # Plot the series
+  plt.plot(timesteps[start:end], values[start:end], format, label=label)
+  plt.xlabel("Time")
+  plt.ylabel("BTC Price")
+  if label:
+    plt.legend(fontsize=14) # make label bigger
+  plt.grid(True)
